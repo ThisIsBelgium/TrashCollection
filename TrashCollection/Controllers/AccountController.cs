3c51954a405735deb66afd7e6d9fe45658635ae4
ThisIsBelgium <Daniel3wheeler@gmail.com>
2017-06-29 12:01:59 -0500

    added roles

--- TrashCollection/Controllers/AccountController.cs
+++ TrashCollection/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace TrashCollection.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Address = model.Address };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- TrashCollection/Models/AccountViewModels.cs
+++ TrashCollection/Models/AccountViewModels.cs
@@ -79,6 +79,19 @@ namespace TrashCollection.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+        [Required]
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
     }
 
     public class ResetPasswordViewModel
